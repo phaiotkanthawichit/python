1e6ecd258c6ef92a9b9ae98b027a16af5845bd6e
phairot.kanthawichit <p.kanthawichit@gmail.com>
2022-08-13 20:41:53 +0700

    5

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,39 @@ class MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("My python "),
-          centerTitle: true,
-        ),
-        body: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Center(
-                  child: Text('PyThon',
-                      style: TextStyle(
-                        fontSize: 50.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue[900],
-                      )),
-                ),
-                Icon(
-                  Icons.add_alert_outlined,
-                  size: 100,
-                  color: Colors.red,
-                )
-              ],
-            ),
-          ],
-        )
-        
-       floatingActionButton: FloatingActionButton(
-        child: Text('Help'),
+      appBar: AppBar(
+        title: Text("My python "),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Center(
+                child: Text('PyThon',
+                    style: TextStyle(
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900],
+                    )),
+              ),
+              Icon(
+                Icons.add_alert_outlined,
+                size: 100,
+                color: Colors.red,
+              )
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
         onPressed: () {
-          print("Click Help");
+          print('Cilck print');
         },
       ),
-    
-   );
+    );
   }
 }
 
